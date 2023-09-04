b70c5f9a1fca1f1e79a18aaf8da50d3b4603688f
Kuma93 <kyuh2002@gmail.com>
2023-09-04 15:43:59 +0900

    update package directory

---
+++ BackEnd/chartflow/src/main/java/com/ssafy/chartflow/exception/EmailExistedException.java
@@ -0,0 +1,7 @@
+package com.ssafy.chartflow.exception;
+
+public class EmailExistedException extends RuntimeException {
+    public EmailExistedException(String email){
+        super(email + "은 이미 가입되어 있습니다.");
+    }
+}
---
+++ BackEnd/chartflow/src/main/java/com/ssafy/chartflow/exception/NotRegisteredException.java
@@ -0,0 +1,7 @@
+package com.ssafy.chartflow.exception;
+
+public class NotRegisteredException extends RuntimeException{
+    public NotRegisteredException(){
+        super("등록이 안된 사용자입니다. 회원가입을 진행해주세요");
+    }
+}
