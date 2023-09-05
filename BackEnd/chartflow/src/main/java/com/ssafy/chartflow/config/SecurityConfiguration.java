4fdb598727dabb5d3fffe7f0258030666fdf2f51
kimtaehyun <taehyun01212@gmail.com>
2023-09-05 10:31:06 +0900

    feat: Spring security 설정

--- BackEnd/chartflow/src/main/java/com/ssafy/chartflow/config/SecurityConfiguration.java
+++ BackEnd/chartflow/src/main/java/com/ssafy/chartflow/config/SecurityConfiguration.java
@@ -1,30 +1,67 @@
 package com.ssafy.chartflow.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity //default security setting을 모두 버리겠다.
 @Configuration
-public class SecurityConfiguration {
-
+public class SecurityConfig {
+    private final String[] allowedUrls = {"/", "/swagger-ui/**", "/v3/**", "*/signup", "*/login"};	// sign-up, sign-in 추가
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-
-//        httpSecurity.httpBasic().disable();
-//        httpSecurity.csrf().disable();
-//        httpSecurity.cors();
-//        httpSecurity.authorizeRequests().antMatchers("/user/login","user/signup","/exception/**","/item/**","/v3/api-docs","/configuration/**","/swagger-ui/**","/webjars/**").permitAll();
-//        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//jwt사용하는 경우
-//
-        return httpSecurity.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf()
+                .disable()
+                .cors()
+                .and()
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("*").permitAll()
+                                //TODO: API 완성됐을 때 상세히 구현. 메인페이지, 로그인, 회원가입 permit해야함.
+                                .anyRequest().authenticated()
+                )
+//                .formLogin(formLogin -> formLogin
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(
+                Arrays.asList("http://localhost:3000")); // 특정 출처 지정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowCredentials(true); // 자격 증명 허용
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
