f936bc9addd709cb6c757bffa79e607ab1cb351e
kimtaehyun <taehyun01212@gmail.com>
2023-09-05 10:35:16 +0900

    feat: Authentication 설정

--- BackEnd/chartflow/src/main/java/com/ssafy/chartflow/config/SecurityConfig.java
+++ BackEnd/chartflow/src/main/java/com/ssafy/chartflow/config/SecurityConfig.java
@@ -3,15 +3,10 @@ package com.ssafy.chartflow.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -25,7 +20,7 @@ import java.util.Arrays;
 @EnableWebSecurity //default security setting을 모두 버리겠다.
 @Configuration
 public class SecurityConfig {
-    private final String[] allowedUrls = {"/", "/swagger-ui/**", "/v3/**", "*/signup", "*/login"};	// sign-up, sign-in 추가
+    private final String[] allowedUrls = {"/", "/swagger-ui/**", "*/signup", "*/login"};	// sign-up, sign-in 추가
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     @Bean
@@ -37,7 +32,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeHttpRequests(requests ->
                         requests.requestMatchers("*").permitAll()
-                                //TODO: API 완성됐을 때 상세히 구현. 메인페이지, 로그인, 회원가입 permit해야함.
+                                //TODO: API 완성됐을 때 상세히 구현. 메인페이지, 로그인, 회원가입 permit해야함. allowedUrl 활용
                                 .anyRequest().authenticated()
                 )
 //                .formLogin(formLogin -> formLogin
