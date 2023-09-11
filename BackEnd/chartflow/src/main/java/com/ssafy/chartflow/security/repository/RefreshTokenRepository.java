7d99728ec1f22f857688f7ec352dd5861ea72ad0
kimtaehyun <taehyun01212@gmail.com>
2023-09-11 16:37:37 +0900

    refactor : 모든 entity 및 repository pk명 id로 수정 및 적용

--- BackEnd/chartflow/src/main/java/com/ssafy/chartflow/security/repository/RefreshTokenRepository.java
+++ BackEnd/chartflow/src/main/java/com/ssafy/chartflow/security/repository/RefreshTokenRepository.java
@@ -3,7 +3,7 @@ package com.ssafy.chartflow.security.repository;
 import com.ssafy.chartflow.security.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface  RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     public RefreshToken findByToken(String token);
 
