589489e47bea34e4656850179cf5d8426f1f9e53
이연주 <legrandreve@naver.com>
2023-09-13 11:00:12 +0900

    Feat: 차트게임 주식매매 기능 구현

--- FrontEnd/chartflow/src/components/BuySell/index.jsx
+++ FrontEnd/chartflow/src/components/BuySell/index.jsx
@@ -1,18 +1,20 @@
-import styles from './BuySell.module.css'
-import Order from '../Order';
+import styles from "./BuySell.module.css";
+import Order from "../Order";
 
 function BuySell() {
-    return (
-        <>
-        <div className={styles.container}>
-            <div className={styles.turn}>turn</div>
-            <div className={styles.order}><Order/></div>
-            <button className={styles.buybtn}>매수</button>
-            <button className={styles.sellbtn}>매도</button>
-            <button className={styles.skip}>건너뛰기</button>
+  return (
+    <>
+      <div className={styles.container1}>
+        <div className={styles.turn}>turn</div>
+        <div className={styles.order}>
+          <Order />
         </div>
-        </>
-    )
+        <button className={styles.buybtn}>매수</button>
+        <button className={styles.sellbtn}>매도</button>
+        <button className={styles.skip}>건너뛰기</button>
+      </div>
+    </>
+  );
 }
 
 export default BuySell;
