b310faf8c1ef8eebb0fe7e5064bc0dad7baee7d8
Kuma93 <kyuh2002@gmail.com>
2023-09-13 14:32:36 +0900

    fix error

--- BackEnd/chartflow/src/main/java/com/ssafy/chartflow/game/repository/GameHistoryStocksRepository.java
+++ BackEnd/chartflow/src/main/java/com/ssafy/chartflow/game/repository/GameHistoryStocksRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameHistoryStocksRepository extends JpaRepository<GameHistoryStocks, Long> {
 
-    GameHistoryStocks findGameHistoryStocksBy
 }
