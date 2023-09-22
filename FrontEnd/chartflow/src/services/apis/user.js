42a4001d38a83b683930ce1adaa03ed2ef828a5d
khs <khs00031@gmail.com>
2023-09-22 17:29:07 +0900

    태현이형이 요청한 존망 프런트

--- FrontEnd/chartflow/src/services/apis/user.js
+++ FrontEnd/chartflow/src/services/apis/user.js
@@ -1,7 +1,10 @@
-import { axiosServer } from "../axiosInstance";
+import { axiosServerWithoutToekn } from "../axiosInstance";
 
 // 로그인
 export const login = async (RequestLoginDto) => {
-  const res = await axiosServer().post(`api/auth/login`, RequestLoginDto);
+  const res = await axiosServerWithoutToekn().post(
+    `api/auth/login`,
+    RequestLoginDto
+  );
   return res.data;
 };
