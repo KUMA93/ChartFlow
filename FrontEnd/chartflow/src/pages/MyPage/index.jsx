27fbed9c5702f1faa1bb0d58b6d0539869c5ff16
ryunayeon <yoonayeon@gmail.com>
2023-09-22 17:35:09 +0900

    feat:Quiz correct 화면 구현

--- FrontEnd/chartflow/src/pages/MyPage/index.jsx
+++ FrontEnd/chartflow/src/pages/MyPage/index.jsx
@@ -1,10 +1,18 @@
 import Header from "../../components/Header";
+import styles from "./MyPage.module.css"
 
 function MyPage() {
   return (
     <>
       <Header />
-      <p>This is MyPage.</p>
+        <div className={styles.nickname}>한강물</div>
+      <div>
+          <div className={styles.profile}></div>
+          <div className={styles.inputBox}>이름</div>
+          <div className={styles.inputBox}>이메일</div>
+      </div>
+        <div className={styles.bottomBox}></div>
+        <div className={styles.btn}>수정</div>
     </>
   );
 }
