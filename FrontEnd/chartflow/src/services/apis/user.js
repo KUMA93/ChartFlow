c36b32fccef8cb43b02e7d5349635bcf9d70d8fa
이연주 <legrandreve@naver.com>
2023-09-25 12:29:40 +0900

    Feat: 회원가입 api 연결

--- FrontEnd/chartflow/src/services/apis/user.js
+++ FrontEnd/chartflow/src/services/apis/user.js
@@ -1,10 +1,19 @@
-import { axiosServerWithoutToekn } from "../axiosInstance";
+import { axiosServerWithoutToken } from "../axiosInstance";
 
 // 로그인
 export const login = async (RequestLoginDto) => {
-  const res = await axiosServerWithoutToekn().post(
-    `api/auth/login`,
+  const res = await axiosServerWithoutToken().post(
+    `auth/login`,
     RequestLoginDto
+  );
+  return res.data;
+};
+
+// 회원가입
+export const join = async (RequestJoinDto) => {
+  const res = await axiosServerWithoutToken().post(
+    `user/regist`,
+    RequestJoinDto
   );
   return res.data;
 };
