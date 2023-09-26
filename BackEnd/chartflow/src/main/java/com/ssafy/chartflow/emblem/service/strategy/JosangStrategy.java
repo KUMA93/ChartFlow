b5383963cceb82d9cbb6287d6d77a80c2133f51a
kimtaehyun <taehyun01212@gmail.com>
2023-09-26 17:45:46 +0900

    feat: emblem 모든 조건 구현 완료

--- BackEnd/chartflow/src/main/java/com/ssafy/chartflow/emblem/service/strategy/JosangStrategy.java
+++ BackEnd/chartflow/src/main/java/com/ssafy/chartflow/emblem/service/strategy/JosangStrategy.java
@@ -1,22 +1,33 @@
 package com.ssafy.chartflow.emblem.service.strategy;
 
 import com.ssafy.chartflow.emblem.dto.UserGameDto;
+import com.ssafy.chartflow.emblem.entity.UserEmblem;
+import com.ssafy.chartflow.game.entity.GameHistory;
+import com.ssafy.chartflow.game.entity.GameTurns;
+
+import java.util.List;
 
 public class JosangStrategy implements GameStrategy{
     @Override
     public boolean checkCondition(UserGameDto userGameDto) {
         // 게임 종료 후 매수매도하고도 수익률 0프로대
         // GameHistory테이블에 진행한 게임에 대한 rate값이 0점대이면 return true
-//        if(userGameDto.getRate()>-1 && userGameDto.getRate()<1)
-//            return true;
-//        else
-//            return false;
-        return false;
+
+        GameHistory gameHistory = userGameDto.getGameHistory();
+        List<GameTurns> gameTurns = gameHistory.getGameTurns();
+        List<UserEmblem> emblems = userGameDto.getEmblems();
+        double rate = gameHistory.getRate();
+
+        boolean hasEmblem = emblems.stream()
+                .anyMatch(emblem -> "물가상승은 조상님이".equals(emblem.getEmblem().getName()));
+        if (hasEmblem) return false;
+
+        return rate < 1.0;
     }
 
     @Override
     public String getTitle() {
-        return "물가상승은 조상님이 내주냐?";
+        return "물가상승은 조상님이";
     }
 
 }
--- BackEnd/chartflow/src/main/java/com/ssafy/chartflow/emblem/service/strategy/LowBoyStrategy.java
+++ BackEnd/chartflow/src/main/java/com/ssafy/chartflow/emblem/service/strategy/LowBoyStrategy.java
@@ -1,16 +1,31 @@
 package com.ssafy.chartflow.emblem.service.strategy;
 
 import com.ssafy.chartflow.emblem.dto.UserGameDto;
+import com.ssafy.chartflow.emblem.entity.UserEmblem;
+import com.ssafy.chartflow.game.entity.GameHistory;
+import com.ssafy.chartflow.game.entity.GameTurns;
+
+import java.util.List;
 
 public class LowBoyStrategy implements GameStrategy{
     @Override
     public boolean checkCondition(UserGameDto userGameDto) {
         // 매수&매도 없음
         // game turns테이블에 진행한 게임에 대한 값이 존재하지 않는 경우 return true
-//        if(userGameDto.getGameTurns().length==0)
-//            return true;
-//        else
-            return false;
+
+        GameHistory gameHistory = userGameDto.getGameHistory();
+        List<GameTurns> gameTurns = gameHistory.getGameTurns();
+        List<UserEmblem> emblems = userGameDto.getEmblems();
+
+        boolean hasEmblem = emblems.stream()
+                .anyMatch(emblem -> "하남자".equals(emblem.getEmblem().getName()));
+        if (hasEmblem) return false;
+
+        for (GameTurns turns: gameTurns){
+            if (turns.getBehavior() != 2)
+                return false;
+        }
+        return true;
     }
 
     @Override
