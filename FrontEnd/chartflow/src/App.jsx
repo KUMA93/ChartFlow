eb861468ccbd38aa4b1043c97cc2c098aa258501
Kuma93 <kyuh2002@gmail.com>
2023-10-04 16:04:20 +0900

    [ALL] 퀴즈 api 수정 및 연동 - 퀴즈 목록 한 개씩  하루에 3개까지 불러오게 수정 - 퀴즈 정답, 오답 페이지 추가 및 라우팅

--- FrontEnd/chartflow/src/App.jsx
+++ FrontEnd/chartflow/src/App.jsx
@@ -14,6 +14,7 @@ import MyPage from "./pages/MyPage";
 import Join from "./pages/Join";
 import NotFound from "./pages/NotFound";
 import QuizCorrect from "./pages/QuizCorrect";
+import QuizWrong from "./pages/QuizWrong";
 import JoinComplete from "./pages/JoinComplete";
 import Forget from "./pages/Forget";
 
@@ -103,7 +104,9 @@ function App() {
               <Route path="/complete" element={<JoinComplete />} />
               <Route path="/forget" element={<Forget />} />
               <Route path="/*" element={<NotFound />} />
-              <Route path="/quizcorrect" element={<QuizCorrect />} />
+              <Route path="/quiz/correct" element={<QuizCorrect />} />
+              <Route path="/quiz/wrong" element={<QuizWrong />} />
+
             </Routes>
           </GameContext.Provider>
         </UserContext.Provider>
--- FrontEnd/chartflow/src/constants/url.js
+++ FrontEnd/chartflow/src/constants/url.js
@@ -1,5 +1,5 @@
 // baseURL: "http://localhost:8080/api",
 export const SERVER_URL = "http://localhost:3000";
 
-// export const API_URL = "http://localhost:8080/api";
-export const API_URL = "http://j9a107.p.ssafy.io:8080/api";
+export const API_URL = "http://localhost:8080/api";
+// export const API_URL = "http://j9a107.p.ssafy.io:8080/api";
