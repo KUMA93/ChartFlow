585a8a8639a189c0291fbe83864817e247d30993
taehyun <taehyun0121@naver.com>
2023-10-05 22:00:58 +0900

    feat: url 변경

--- FrontEnd/chartflow/src/constants/url.js
+++ FrontEnd/chartflow/src/constants/url.js
@@ -2,5 +2,5 @@
 export const SERVER_URL = "http://localhost:3000";
 // export const SERVER_URL = "http://j9a107.p.ssafy.io:8080/api";
 
-export const API_URL = "http://localhost:8080/api";
-// export const API_URL = "http://j9a107.p.ssafy.io:8080/api";
+//export const API_URL = "http://localhost:8080/api";
+export const API_URL = "http://j9a107.p.ssafy.io:8080/api";
