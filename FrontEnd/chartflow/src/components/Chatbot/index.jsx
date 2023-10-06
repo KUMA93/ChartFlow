6deea3849228a397c517d18b4f59b19221acf2a9
kimtaehyun <taehyun01212@gmail.com>
2023-10-06 10:30:19 +0900

    feat: chatbot 연동 업로드

--- FrontEnd/chartflow/src/components/Chatbot/index.jsx
+++ FrontEnd/chartflow/src/components/Chatbot/index.jsx
@@ -1,46 +1,55 @@
-import s from "./Chatbot.module.css"
-import { useState } from 'react';
+import s from "./Chatbot.module.css";
+import { useState, useEffect } from 'react';
+import { chatbot } from "../../services/apis/chatbot";
 
-function Chatbot({ isOpen, closeModal}) {
-
+function Chatbot({ isOpen, closeModal }) {
   const [inputText, setInputText] = useState('');
-  const [displayText, setDisplayText] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
 
-  const handleEnterPress = (event) => {
+  const handleEnterPress = async (event) => {
     if (event.key === 'Enter') {
-      setDisplayText(inputText);
+      const newChatHistory = [...chatHistory, { type: 'user', text: inputText }];
+      setChatHistory(newChatHistory);
+      setInputText('');
+      setIsLoading(true);
+
+      // API 호출
+      const response = await chatbot({ text: inputText });
+      setIsLoading(false);
+
+      setChatHistory([...newChatHistory, { type: 'bot', text: response }]);
     }
   };
 
   return (
-      <div style={{ display: isOpen ? "block": "none" }}>
-        <div className={s.shade}>
-          <div className={s.modalChat}>
-            <div className={s.font}>
-              무엇이 궁금한가요?
-            </div>
-            <div className={s.chatCon}>
-              <div className={s.inputText}>
-                {displayText}
+    <div style={{ display: isOpen ? "block": "none" }}>
+      <div className={s.shade}>
+        <div className={s.modalChat}>
+          {isLoading && <div className={s.loading}>Loading...</div>}
+          <div className={s.chatCon}>
+            {chatHistory.map((chat, index) => (
+              <div key={index} className={chat.type === 'user' ? s.userText : s.botText}>
+                {chat.text}
               </div>
-            </div>
-            
-            <input 
-              type="text"
-              className={s.chat}
-              value={inputText}
-              onChange={handleInputChange}
-              onKeyDown={handleEnterPress}
-            />
-            <button className={s.closeBtn} onClick={closeModal}>닫기</button>
+            ))}
           </div>
+          <input 
+            type="text"
+            className={s.chat}
+            value={inputText}
+            onChange={handleInputChange}
+            onKeyDown={handleEnterPress}
+          />
+          <button className={s.closeBtn} onClick={closeModal}>닫기</button>
         </div>
       </div>
+    </div>
   );
 }
 
-export default Chatbot
+export default Chatbot;
